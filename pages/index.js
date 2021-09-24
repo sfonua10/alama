d3081e7926e44091efe497b25042eaeb6f81df84
Saia Fonua <saiafonua@gmail.com>
2021-09-23 22:22:02 -0600

    v1 homepage

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,17 @@ import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
+  { name: "Resume", href: "#" },
+  { name: "Skills", href: "#" },
+  { name: "About", href: "#" },
+  { name: "Contact", href: "#" },
 ];
 
 export default function Home() {
   return (
     <div className="relative bg-gray-50 overflow-hidden">
       <Head>
-        <title>Create Next App</title>
+        <title>Alma Fonua</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div
@@ -97,7 +97,8 @@ export default function Home() {
               aria-label="Global"
             >
               <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-                <div className="flex items-center justify-between w-full md:w-auto">
+                <h1>Alma Fonua</h1>
+                {/* <div className="flex items-center justify-between w-full md:w-auto">
                   <a href="#">
                     <span className="sr-only">Workflow</span>
                     <img
@@ -112,7 +113,7 @@ export default function Home() {
                       <MenuIcon className="h-6 w-6" aria-hidden="true" />
                     </Popover.Button>
                   </div>
-                </div>
+                </div> */}
               </div>
               <div className="hidden md:flex md:space-x-10">
                 {navigation.map((item) => (
@@ -192,16 +193,61 @@ export default function Home() {
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">Data to enrich your</span>{" "}
+              {/* <span className="block xl:inline">Data to enrich your</span>{" "} */}
               <span className="block text-indigo-600 xl:inline">
-                online business
+                Alma Fonua
               </span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Independent Broker
             </p>
+
+            <div className="space-y-6">
+              <img
+                className="mx-auto h-72 w-72 rounded-full xl:w-56 xl:h-56 mt-6"
+                src="/alma.jpeg"
+                alt=""
+              />
+              <div className="space-y-2">
+                <div className="text-lg leading-6 font-medium space-y-1">
+                  <h3>Alma Fonua</h3>
+                  <p className="text-indigo-600">Independent Broker</p>
+                </div>
+                <ul role="list" className="flex justify-center space-x-5">
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-500" href="https://twitter.com/alama005">
+                      <span className="sr-only">Twitter</span>
+                      <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-400 hover:text-gray-500" href="https://www.linkedin.com/in/alma-fonua/">
+                      <span className="sr-only">LinkedIn</span>
+                      <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+{/* 
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
                 <a
@@ -219,7 +265,7 @@ export default function Home() {
                   Live demo
                 </a>
               </div>
-            </div>
+            </div> */}
           </div>
         </main>
       </div>
